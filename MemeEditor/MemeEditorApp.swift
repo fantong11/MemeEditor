0f99eaed9a173002ed3ae26b0bbc82a5606a558e
Fantong <fankaihsiang11@gmail.com>
2022-03-05 20:22:38 +0800

    Initial Commit

---
+++ MemeEditor/MemeEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeEditorApp.swift
+//  MemeEditor
+//
+//  Created by 范桶 on 2022/3/5.
+//
+
+import SwiftUI
+
+@main
+struct MemeEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
