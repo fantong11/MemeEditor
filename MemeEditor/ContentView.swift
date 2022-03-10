d2d0e8314e671a377858dae447ed4eff192cad46
Fantong <fankaihsiang11@gmail.com>
2022-03-10 10:13:55 +0800

    Project init

--- MemeEditor/ContentView.swift
+++ MemeEditor/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Button("Choose an image from gallery") {
+                showingImagePicker = true
+            }
             .padding()
+            image?
+                .resizable()
+                .scaledToFit()
+                .padding()
+        }
+        .navigationTitle("Choose an image")
+        .onChange(of: inputImage) { _ in
+            loadImage()
+        }
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker(image: $inputImage)
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else {
+            return
+        }
+        print(inputImage)
+        image = Image(uiImage: inputImage)
+        
     }
 }
 
---
+++ MemeEditor/ImagePicker.swift
@@ -0,0 +1,55 @@
+//
+//  ImagePicker.swift
+//  MemeEditor
+//
+//  Created by 范桶 on 2022/3/6.
+//
+
+import PhotosUI
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = context.coordinator
+        return picker
+        
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        // 上面ImagePicker的instance
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            guard let provider = results.first?.itemProvider else {
+                return
+            }
+            
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+            
+            
+        }
+    }
+}
